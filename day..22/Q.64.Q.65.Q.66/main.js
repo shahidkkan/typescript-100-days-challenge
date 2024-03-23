806949c6ea930c81a6ded10f71cb1477dfe9e258
Shahid khan <161056935+shahidkkan@users.noreply.github.com>
2024-03-24 02:13:00 +0500

    first comment

---
+++ day..22/Q.64.Q.65.Q.66/main.js
@@ -0,0 +1,33 @@
+"use strict";
+/*/Question 64: Combine Strings and Numbers:
+
+// This function mixes a text and a number into one text
+function combineStringAndNumber(text: string, number: number): string {
+    // Joins the text and number into a single text
+    return text + number;
+}
+
+// Trying it out with "Age: " and 30
+console.log(combineStringAndNumber("Age: ", 20)); // Shows "Age: 20"
+// Here, we put together the text and number.
+
+//Question 65: Determine the Remainder:
+
+// This function finds the leftover of dividing two numbers
+function remainder(num1: number, num2: number): number {
+    // Gives back the leftover of num1 divided by num2
+    return num1 % num2;
+}
+
+// Trying it with 5 divided by 2
+console.log(remainder(5, 2)); // Shows 1
+// This tells us the leftover, which is 1 here.*/
+//Question 66: Logical AND Verification: 
+// This function sees if both inputs are true
+function checkBothTrue(val1, val2) {
+    // Only says true if both val1 and val2 are true
+    return val1 && val2;
+}
+// Trying it with true and false
+console.log(checkBothTrue(true, false)); // Shows false
+// It checks two things, but since one is false, the answer is false.*/cla
