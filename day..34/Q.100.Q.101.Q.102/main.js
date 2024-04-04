4673d7ef41c965371c54dc03785e87d840806c0c
Shahid khan <161056935+shahidkkan@users.noreply.github.com>
2024-04-05 02:00:06 +0500

    first comment

---
+++ day..34/Q.100.Q.101.Q.102/main.js
@@ -0,0 +1,24 @@
+"use strict";
+/*/Question 100: Use the JavaScript Math object to find the square root of 120.
+
+// Finds the square root of 120 using Math.sqrt()
+const squareRoot: number = Math.sqrt(120);
+
+console.log(squareRoot); // Outputs: 10
+// This line effectively calculates and shows the square root of 144.
+
+//Question 101: Generate a random integer between 1 and 10.
+
+// Generates a random integer between 1 and 10
+function getRandomInt(): number {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(getRandomInt()); // Outputs a random integer between 1 and 10
+// This function combines Math.random() with Math.floor() to create a random integer within our range.*/
+Object.defineProperty(exports, "__esModule", { value: true });
+//Question 102: Calculate and log the absolute difference between the number -5 and 5.
+// Calculates the absolute difference between -5 and 5
+const difference = Math.abs(-5 - 5);
+console.log(difference); // Outputs: 10
+// This line shows the absolute value of the difference between -5 and 5, ignoring the sign.
