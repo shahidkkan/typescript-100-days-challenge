a7513742bc6d91ef2f543f8cbba0f4d524e7257f
Shahid khan <161056935+shahidkkan@users.noreply.github.com>
2024-04-25 23:53:46 +0500

    first commint

---
+++ day..54/Q.day 54/alert.js
@@ -0,0 +1,15 @@
+let admin;
+let name;
+name = "shahid";
+admin = name;
+alert(admin);
+
+let ourPlanet = "shahid";
+let currentUser = "khan";
+
+const myBirthDay = '12 set,2004';
+const myAge = someCode(myBirthDay);
+
+//we can also use uppercase for const variables
+const MYBIRTHDAY = '30 July,1996';
+const MYAGE = someCode(MYBIRTHDAY);
